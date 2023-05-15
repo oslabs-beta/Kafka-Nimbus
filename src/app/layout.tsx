0717c74fb8b1c28dfa824716f020be712cf200a5
Ariel Lin <al2248@cornell.edu>
2023-05-15 01:44:41 -0400

    NextOAuth works with Next13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,44 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
-import { Provider } from "./components/provider";
+// import { SessionProvider } from "next-auth/react";
+import { Provider } from "./components/Provider";
 import '../styles/globals.css';
 
+// import { api } from "~/utils/api";
 
-import { api } from "~/utils/api";
+// const MyApp: AppType<{ session: Session | null }> = ({
+//   Component,
+//   pageProps: { session, ...pageProps },
+// }) => {
+//   return (
+//     <SessionProvider session={session}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//   );
+// };
 
-import "~/styles/globals.css";
+/**
+ * Need to export SessionProvider in Next13 because it is a context Provider using react Context so it needs to be a client component. 
+ * Originally you designed your own custom top level MyApp component and then wrapped in it SessionProvider
+ * Next13 uses RootLayout (outermost layer of our application). Must contain html and body tags
+ * Can add header and footer here so all pages have those shared components
+ */
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <Provider session={session}>
-      <Component {...pageProps} />
-    </Provider>
+    <html lang="en">
+      <body>
+        <Provider>
+          {children}
+        </Provider>
+      </body>
+    </html>
   );
-};
+}
 
-export default api.withTRPC(MyApp);
+// export default api.withTRPC(MyApp);
+// export default MyApp;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-"use client";
-
 import { type NextPage } from "next";
 import Link from "next/link";
-import { Metadata } from 'next';
-import { signIn, signOut, useSession } from "next-auth/react";
-
-import { api } from "~/utils/api";
+import { type Metadata } from 'next';
+import { AuthShowcase } from "./components/AuthShowcase";
+// import { api } from "~/utils/api";
 
 export const metadata: Metadata = {
   title: 'Kafka-Nimbus',
@@ -16,7 +13,7 @@ export const metadata: Metadata = {
 }
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -51,7 +48,7 @@ const Home: NextPage = () => {
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+              {/* {hello.data ? hello.data.greeting : "Loading tRPC query..."} */}
             </p>
             <AuthShowcase />
           </div>
@@ -62,27 +59,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
