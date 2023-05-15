1dadddec87901b4fa97e8561b557669b329daa2f
Ariel Lin <al2248@cornell.edu>
2023-05-15 19:51:03 -0400

    Temp fix to weird message error about AuthShowcase using useSession

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { type NextPage } from "next";
 import Link from "next/link";
-import { AuthShowcase } from "./components/AuthShowcase";
+// import { AuthShowcase } from "./components/AuthShowcase";
 import { api } from "~/utils/api";
 import { withTRPC } from '@trpc/next';
 import { appRouter } from '../server/api/root';
@@ -56,3 +56,31 @@ const Home: NextPage = () => {
 
 // export default Home;
 export default api.withTRPC(Home);
+
+//Removes weird bug when adding AuthShowcase directly into pages.tsx instead of exporting and importing this component.
+// [next-auth]: `useSession` must be wrapped in a <SessionProvider />
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export const AuthShowcase: React.FC = () => {
+  const { data: sessionData } = useSession();
+
+  // const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+  //   undefined, // no input
+  //   { enabled: sessionData?.user !== undefined },
+  // );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {/* {secretMessage && <span> - {secretMessage}</span>} */}
+      </p>
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+};
