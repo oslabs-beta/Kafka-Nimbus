af076244921944d54d3442736aa55d5e0602b70b
paulrvach <paulrvach@gmail.com>
2023-05-16 10:37:24 -0400

    added a navbar and cluster-dash page

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React, { useState } from "react";
+import logo from "../../../public/logoword.svg";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+
+const NavBar = () => {
+  const { data: sessionData } = useSession();
+  const [profileModal, setProfileModal] = useState(false);
+
+  const profileModalHandler = () => {
+    setProfileModal(!profileModal);
+  };
+
+  const dropDownMenu = (
+    <ul
+      tabIndex={0}
+      className="menu menu-compact  dropdown-content bg-base-100 rounded-box mt-3 w-52 p-2 shadow"
+    >
+      <li>
+        <a className="justify-between">
+          Profile
+          <span className="badge">New</span>
+        </a>
+      </li>
+      <li>
+        <a>Settings</a>
+      </li>
+      <li>
+        <a>Logout</a>
+      </li>
+    </ul>
+  );
+
+  return (
+    <div className="navbar bg-base-100 mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8">
+      <div className="flex cursor-pointer flex-row align-middle">
+        <Image src={logo} alt="logo" className="mr-2 h-8 w-8" />
+        <a className="btn btn-ghost text-xl normal-case">Kafka Nimbus</a>
+      </div>
+
+      <div className="overflow-hidden rounded-full hover:bg-slate-300">
+        <Image
+          width="34"
+          height="34"
+          src={sessionData ? sessionData.user.image : logo}
+          alt="profile-pic"
+          className=""
+          onClick={profileModalHandler}
+        />
+      </div>
+      {profileModal ? dropDownMenu : null}
+    </div>
+  );
+};
+
+export default NavBar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 // import { SessionProvider } from "next-auth/react";
 import { Provider } from "./components/Provider";
-import '../styles/globals.css';
+import "../styles/globals.css";
+import NavBar from "./components/NavBar";
 
 // import { api } from "~/utils/api";
 
@@ -18,7 +19,7 @@ import '../styles/globals.css';
 // };
 
 /**
- * Need to export SessionProvider in Next13 because it is a context Provider using react Context so it needs to be a client component. 
+ * Need to export SessionProvider in Next13 because it is a context Provider using react Context so it needs to be a client component.
  * Originally you designed your own custom top level MyApp component and then wrapped in it SessionProvider
  * Next13 uses RootLayout (outermost layer of our application). Must contain html and body tags
  * Can add header and footer here so all pages have those shared components
@@ -33,6 +34,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Provider>
+          <NavBar />
           {children}
         </Provider>
       </body>
