757693d7488ae1117485dec208a003f1d8ee85ed
paulrvach <paulrvach@gmail.com>
2023-05-18 20:09:56 -0400

    createVPC tRPC connector

--- src/app/(clusters)/(components)/StoragePerBroker.tsx
+++ src/app/(clusters)/(components)/StoragePerBroker.tsx
@@ -5,9 +5,10 @@ import { setStoragePerBroker } from '~/app/redux/features/createClusterSlice';
 
 interface ProviderProps {
   inFocusHandler: (string: string) => void;
+  setCallDatabase: (boolean: boolean) => void
 }
 
-const StoragePerBroker: React.FC<ProviderProps> = ({ inFocusHandler }) => {
+const StoragePerBroker: React.FC<ProviderProps> = ({ inFocusHandler, setCallDatabase }) => {
   const dispatch = useAppDispatch();
   const [numValue, setNumValue] = useState<number>(0);
   const { createCluster } = useAppSelector((state) => state);
@@ -16,6 +17,8 @@ const StoragePerBroker: React.FC<ProviderProps> = ({ inFocusHandler }) => {
     event.preventDefault();
     console.log(createCluster);
     dispatch(setStoragePerBroker(numValue));
+    setCallDatabase(true)
+    inFocusHandler('loading')
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
--- src/app/(clusters)/create-cluster/page.tsx
+++ src/app/(clusters)/create-cluster/page.tsx
@@ -1,39 +1,73 @@
-"use client";
-import React, { useState } from "react";
-import CloudProvider from "../(components)/CloudProvider";
-import AwsSecrets from "../(components)/AwsSecrets";
-import RegionInput from "../(components)/RegionInput";
-import ClusterNameInput from "../(components)/ClusterNameInput";
-import BrokerCountInput from "../(components)/BrokerCounterInput";
-import StoragePerBroker from "../(components)/StoragePerBroker";
-import ClusterSize from "../(components)/ClusterSize";
+'use client';
+import React, { useState } from 'react';
+import { useAppSelector } from '~/app/redux/hooks';
+import CloudProvider from '../(components)/CloudProvider';
+import AwsSecrets from '../(components)/AwsSecrets';
+import RegionInput from '../(components)/RegionInput';
+import ClusterNameInput from '../(components)/ClusterNameInput';
+import BrokerCountInput from '../(components)/BrokerCounterInput';
+import StoragePerBroker from '../(components)/StoragePerBroker';
+import ClusterSize from '../(components)/ClusterSize';
+import ClusterLoadingState from '../(components)/ClusterLoadingState';
+
+// TRPC IMPORTS
+import { api } from '~/utils/api';
 
 type ComponentState = {
-  inFocus: string
-}
+  inFocus: string;
+};
 
 const CreateClusterPage = () => {
-  const [inFocus, setInFocus] = useState<ComponentState['inFocus']>("provider");
+  const [inFocus, setInFocus] = useState<ComponentState['inFocus']>('provider');
+  const [callDatabase, setCallDatabase] = useState<boolean>(false);
+  const { createCluster } = useAppSelector((state) => state);
 
   const inFocusHandler = (string: string) => {
-    setInFocus(string)
-  }
+    setInFocus(string);
+  };
+
+  const createVPC = () => {
+    const {
+      awsId,
+      awsSecret,
+      brokerNumbers,
+      region,
+      clusterName,
+      provider,
+      storagePerBroker,
+      clusterSize,
+    } = createCluster;
+
+    const response = api.createVPC.createVPC.useQuery({
+      aws_access_key_id: awsId,
+      aws_secret_access_key: awsSecret,
+      id: ,
+      region: region
+    });
+  };
 
   switch (inFocus) {
-    case "provider":
-      return <CloudProvider inFocusHandler={inFocusHandler}/>;
-    case "aws":
-      return <AwsSecrets inFocusHandler={inFocusHandler}/>;
-    case "region":
-      return <RegionInput inFocusHandler={inFocusHandler}/>
-    case "name":
-      return <ClusterNameInput inFocusHandler={inFocusHandler}/>
-    case "size":
-      return <ClusterSize inFocusHandler={inFocusHandler}/>
-    case "brokers":
-      return <BrokerCountInput inFocusHandler={inFocusHandler}/>
-    case "storage":
-      return <StoragePerBroker inFocusHandler={inFocusHandler}/>
+    case 'provider':
+      return <CloudProvider inFocusHandler={inFocusHandler} />;
+    case 'aws':
+      return <AwsSecrets inFocusHandler={inFocusHandler} />;
+    case 'region':
+      return <RegionInput inFocusHandler={inFocusHandler} />;
+    case 'name':
+      return <ClusterNameInput inFocusHandler={inFocusHandler} />;
+    case 'size':
+      return <ClusterSize inFocusHandler={inFocusHandler} />;
+    case 'brokers':
+      return <BrokerCountInput inFocusHandler={inFocusHandler} />;
+    case 'storage':
+      return (
+        <StoragePerBroker
+          inFocusHandler={inFocusHandler}
+          setCallDatabase={setCallDatabase}
+        />
+      );
+    case 'loading':
+      return <ClusterLoadingState inFocusHandler={inFocusHandler} />;
   }
 };
 
