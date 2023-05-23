dbbbe48993fa7b995c824e4cf617816843ec57d0
Ariel Lin <al2248@cornell.edu>
2023-05-23 19:59:15 -0400

    Added describecluster information

--- src/server/api/routers/metricsRouter.ts
+++ src/server/api/routers/metricsRouter.ts
@@ -1,99 +1,83 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Kafka } from "kafkajs";
 import { env } from "~/env.mjs";
-import { prisma } from '../../db'
-import { KafkaClient, DescribeClusterCommand } from '@aws-sdk/client-kafka';
+import { prisma } from '../../db';
+import { z } from 'zod';
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
+import { KafkaClient, DescribeClusterCommand, type DescribeClusterCommandInput, type DescribeClusterCommandOutput } from '@aws-sdk/client-kafka';
+import { Kafka, logLevel, type Admin, type Producer, type Consumer } from 'kafkajs';
+import { createMechanism } from '@jm18457/kafkajs-msk-iam-authentication-mechanism';
 
-const brokers: string[] = ['b-1-public.prometheus4acae81144d6.0my001.c2.kafka.us-east-2.amazonaws.com:9196', 'b-2-public.prometheus4acae81144d6.0my001.c2.kafka.us-east-2.amazonaws.com:9196']
-const region = 'us-east-2';
-//jackson/clusterRouter
-//getting broker/boostrap public endpoints
-//username: user's id in prisma database (input.id)
-//password: kafka-nimbus
-// const user_id: string | undefined = ; //get user id
-const request = { body: { cluster_id: 'cluster id' } };
+//metricRouter contains a getClusterInformation procedure that returns all of a cluster's information given the unique cluster id of the document in mongoDB
+export const metricRouter = createTRPCRouter({
+  getClusterInformation: publicProcedure
+    .input(z.object({
+      cluster_id: z.string()
+    }))
+    .mutation(async ({ input }) => {
+      try {
+        //get Cluster Information from the database
+        const clusterInfo = await prisma.cluster.findUnique({
+          where: {
+            id: input.cluster_id,
+          },
+          include: {
+            User: true
+          }
+        });
+        if (!clusterInfo) {
+          throw new Error('GCI error: Cluster does not exist in the database');
+        }
 
-const client = new KafkaClient(
-  { region }
-);
+        //get bootstrap public endpoints
+        const brokers = ['b-2-public.iamauth.s9rrkn.c2.kafka.us-east-2.amazonaws.com:9198', 'b-1-public.iamauth.s9rrkn.c2.kafka.us-east-2.amazonaws.com:9198'];
+        // const brokers = clusterInfo.bootStrapServer;
 
-const kafka: Kafka = new Kafka({
-  clientId: 'my-app',
-  brokers,
-  ssl: true,
-  sasl: {
-    mechanism: 'scram-sha-512',
-    username: process.env.AWS_SASL_USERNAME ? process.env.AWS_SASL_USERNAME : "",
-    password: 'kafka-nimbus',
-  },
-});
+        const accessKeyId = clusterInfo.User.awsAccessKey;
+        const secretAccessKey = clusterInfo.User.awsSecretAccessKey;
+        const region = clusterInfo.User.region;
 
-console.log('Successfully connected to cluster');
-const admin = kafka.admin();
+        const authParams = {
+          region,
+          credentials: {
+            accessKeyId, secretAccessKey
+          }
+        }
 
-const runAdmin = async (): Promise<void> => {
-  try {
-    await admin.connect();
-    console.log('Admin has connected');
-    const clusterData = await admin.describeCluster();
-    console.log('ClusterData: ', clusterData);
-    await admin.disconnect();
-    console.log('Admin has disconnected');
-  } catch (err) {
-    console.error('Error in admin: ', err);
-  }
-}
+        //AWS kafka client
+        const client = new KafkaClient(
+          authParams
+        );
 
-const clusterData = async (): Promise<void> => {
-  try {
-    const clusterResponse = await prisma.cluster.findUnique({
-      where: {
-        id: request.body.cluster_id
+        //KafkaJS client
+        const kafka: Kafka = new Kafka({
+          clientId: 'my-app',
+          brokers,
+          logLevel: logLevel.ERROR,
+          ssl: true,
+          sasl: createMechanism(authParams)
+        });
+        console.log('KJS: Successfully connected to cluster');
+
+        //Getting DescribeCluster
+        const input: DescribeClusterCommandInput = {
+          ClusterArn: clusterInfo?.kafkaArn ? clusterInfo?.kafkaArn : "",
+        };
+
+        const command = new DescribeClusterCommand(input);
+        const response: DescribeClusterCommandOutput = await client.send(command);
+        const cluster = response.ClusterInfo;
+        //response.ClusterName, response.CreationTime, response.CurrentBrokerSoftwareInfo.KafkaVersion, response.InstanceType,
+        //response.NumberOfBrokerNodes, response.State
+        if (!cluster) throw new Error('Cluster does not have info');
+        const { ClusterName, CreationTime, CurrentBrokerSoftwareInfo, NumberOfBrokerNodes, State } = cluster;
+
+
+        const admin: Admin = kafka.admin();
+        console.log('KJS: Successfully created admin');
+
+      } catch (err) {
+        console.log('Error occurred in metricRouter getClusterInformation: ', err);
       }
-    });
-
-    const input = { // DescribeClusterRequest
-      ClusterArn: clusterResponse?.kafkaArn,
-    };
-
-    const command = new DescribeClusterCommand(input);
-    const response = await client.send(command);
-    //response.ClusterName, response.CreationTime, response.CurrentBrokerSoftwareInfo.KafkaVersion, response.InstanceType,
-    //response.NumberOfBrokerNodes, response.State
-
-  } catch (err) {
-    throw new Error('Could not find cluster inside DB');
-  }
-
-
-};
-
-
-
-const producer = kafka.producer();
-
-const runProducer = async (): Promise<void> => {
-  try {
-    await producer.connect();
-    console.log('Producer has connected');
-
-    // Produce messages or perform other operations here
-    await producer.disconnect();
-    console.log('Producer has disconnected');
-  } catch (err) {
-    console.error('Error in producer:', err);
-  }
-};
-
-runAdmin().then(() =>
-  console.log('Successfully completed admin run')
-).catch((err) =>
-  console.log('Error occurred when running admin: ', err)
-);
-
-runProducer().then(() =>
-  console.log('Successfully completed producer run')
-).catch((err) =>
-  console.log('Error occurred when running producer: ', err)
-);
+    })
+});
