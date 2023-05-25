a30a865ba6da84647a8bc13d467a5b6d77a15f38
Ariel Lin <al2248@cornell.edu>
2023-05-25 08:47:18 -0400

    completed consumer group

--- src/server/api/routers/metricsRouter.ts
+++ src/server/api/routers/metricsRouter.ts
@@ -6,21 +6,33 @@ import { prisma } from '../../db';
 import { z } from 'zod';
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
 import { KafkaClient, DescribeClusterCommand, type DescribeClusterCommandInput, type DescribeClusterCommandOutput } from '@aws-sdk/client-kafka';
-import { Kafka, logLevel, type ITopicMetadata, ConfigResourceTypes, type Admin, type Producer, type Consumer, type DescribeConfigResponse } from 'kafkajs';
+import { Kafka, logLevel, AssignerProtocol, type ITopicMetadata, ConfigResourceTypes, type Admin, type Producer, type Consumer, type DescribeConfigResponse } from 'kafkajs';
 import { createMechanism } from '@jm18457/kafkajs-msk-iam-authentication-mechanism';
+import { group } from "console";
 
 //metricRouter contains a getClusterInformation procedure that returns all of a cluster's information given the unique cluster id of the document in mongoDB
 export const metricRouter = createTRPCRouter({
   getClusterInformation: publicProcedure
     .input(z.object({
-      cluster_id: z.string()
+      cluster: z.string()
     }))
-    .mutation(async ({ input }) => {
+    .query(async ({ input }) => {
       try {
+        interface ResponseBody {
+          Metrics: any;
+          Topics: any[];
+          ConsumerGroups: any[];
+        }
+        const response: ResponseBody = {
+          Metrics: {},
+          Topics: [],
+          ConsumerGroups: [],
+        };
+
         //get Cluster Information from the database
         const clusterInfo = await prisma.cluster.findUnique({
           where: {
-            id: input.cluster_id,
+            id: input.cluster,
           },
           include: {
             User: true
@@ -52,7 +64,7 @@ export const metricRouter = createTRPCRouter({
 
         //KafkaJS client
         const kafka: Kafka = new Kafka({
-          clientId: 'my-app',
+          clientId: `Cluster${input.cluster}`,
           brokers,
           logLevel: logLevel.ERROR,
           ssl: true,
@@ -61,24 +73,32 @@ export const metricRouter = createTRPCRouter({
         console.log('KJS Successfully connected to cluster');
 
 
+        //GETTING METRICS
 
-        //Getting Cluster Dashboard Information from MSK
-        const input: DescribeClusterCommandInput = {
+        //Cluster Dashboard Information from MSK
+        const commInput: DescribeClusterCommandInput = {
           ClusterArn: clusterInfo?.kafkaArn ? clusterInfo?.kafkaArn : "",
         };
 
-        const command = new DescribeClusterCommand(input);
+        const command = new DescribeClusterCommand(commInput);
         console.log('Sending DescribeClusterCommand through MSK client');
         const descClusterResponse: DescribeClusterCommandOutput = await client.send(command);
         const cluster = descClusterResponse.ClusterInfo;
-        //response.ClusterName, response.CreationTime, response.CurrentBrokerSoftwareInfo.KafkaVersion, response.InstanceType,
-        //response.NumberOfBrokerNodes, response.State
+        //cluster.ClusterName, cluster.CreationTime, cluster.CurrentBrokerSoftwareInfo.KafkaVersion, cluster.InstanceType,
+        //cluster.NumberOfBrokerNodes, cluster.State
         if (!cluster) throw new Error('Error: MSK Cluster does not have info');
         const { ClusterName, CreationTime, CurrentBrokerSoftwareInfo, NumberOfBrokerNodes, State } = cluster;
 
+        response.Metrics = {
+          ClusterName,
+          CreationTime,
+          KafkaVersion: CurrentBrokerSoftwareInfo?.KafkaVersion,
+          NumberOfBrokerNodes,
+          State
+        };
 
 
-        //Getting Topics
+        //GETTING TOPICS
         const admin: Admin = kafka.admin();
         console.log('KJS: Successfully created admin');
 
@@ -89,39 +109,43 @@ export const metricRouter = createTRPCRouter({
         //Helper function to get Topic config information
         const descTopicConfig = async function (name: string): DescribeConfigResponse {
           try {
-            const response = await admin.describeConfigs({
+            const responseConfig = await admin.describeConfigs({
               includeSynonyms: false,
               resources: [
                 {
                   type: ConfigResourceTypes.TOPIC,
                   name,
-                  configNames: ['cleanup.policy', 'retention.ms', 'message.downconversion.enable', 'message.format.version', 'max.compaction.lag.ms', 'file.delete.delay.ms', 'max.message.bytes', 'index.interval.bytes'],
+                  configNames: ['cleanup.policy', 'retention.ms', 'message.format.version', 'file.delete.delay.ms', 'max.message.bytes', 'index.interval.bytes'],
                 },
               ]
             });
-            return response;
+            return responseConfig;
           } catch (err) {
             console.log('Error occurred in descTopicConfig');
           }
-
         }
 
         //Processing each topic's data and storing it in an array topicsData
+
         const kTopicsData: ITopicMetadata[] = fetchTopicMetaResponse.topics;
 
         const topicsData = [];
         kTopicsData.forEach(async (topic: ITopicMetadata) => {
           try {
             if (topic.name !== '__consumer_offsets' || topic.name !== '__amazon_msk_canary') {
-              //add list of Consumer Groups subscribed to this topic
+              //add list of Consumer Groups subscribed to this topic [TODO]
 
               //add config description to topic
-              //must put await for this function else it will cause an error: KafkaJSConnectionError: Connection error: Client network socket disconnected before secure TLS connection was established
+              //[WEIRD BUG] must put await for this function else it will cause an error: KafkaJSConnectionError: Connection error: Client network socket disconnected before secure TLS connection was established
               const configData = await descTopicConfig(topic.name);
+
+              //get offsetdata for each Topic
+              const offSetData = await admin.fetchTopicOffsets(topic.name);
 
               topicsData.push({
                 ...topic,
-                configuration: configData.resources[0].configEntries,
+                config: configData.resources[0].configEntries,
+                offsets: offSetData,
               });
             }
           } catch (err) {
@@ -129,14 +153,39 @@ export const metricRouter = createTRPCRouter({
           }
         });
 
-        //Getting Consumer Groups
+        response.Topics = topicsData;
 
+        //GETTING CONSUMER GROUPS
+        const listGroups = await admin.listGroups();
+        //getting list of consumer group Ids
+        const groupIds = listGroups.groups.map(group => group.groupId);
+        console.log('Consumer Groups: ', groupIds);
 
-
-        //Getting Metrics
-
-
-
+        const groupsData = [];
+        const describeGroupsResponse = await admin.describeGroups(groupIds);
+        console.log('Consumer Groups Descriptions: ', describeGroupsResponse);
+        const descGroups = describeGroupsResponse.groups;
+        //for each group in array add to members and subscribedTopics List
+        descGroups.forEach(group => {
+          const { groupId, protocol, state, members } = group;
+          const membersId = [];
+          const subscribedTopics = [];
+          if (members.length > 0) {
+            membersId = members.map(member => member.memberId);
+            const memberAssignment = AssignerProtocol.MemberAssignment.decode(members[0].memberAssignment);
+            for (const key in memberAssignment) {
+              subscribedTopics.push(key);
+            }
+          }
+          groupsData.push({
+            groupId,
+            protocol,
+            state,
+            members: membersId,
+            subscribedTopics,
+          });
+        })
+        response.ConsumerGroups = groupsData;
 
       } catch (err) {
         console.log('Error occurred in metricRouter getClusterInformation: ', err);
