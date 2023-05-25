15b3e395830829712d88bedb410c82bc46d29861
paulrvach <paulrvach@gmail.com>
2023-05-25 16:17:08 -0400

    added graidents

--- src/app/components/ClusterCard.tsx
+++ src/app/components/ClusterCard.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
-import * as React from "react";
-import { useRouter } from "next/navigation";
+'use client';
+import * as React from 'react';
+import { useRouter } from 'next/navigation';
 
 export interface cardCluster {
   cluster: {
@@ -11,26 +11,32 @@ export interface cardCluster {
   };
 }
 
-
 export default function ClusterCard({ cluster }: cardCluster) {
   const router = useRouter();
 
-const routeToCluster = () => {
-  router.push(`${cluster.id}/dashboard`);
-};
+  const routeToCluster = () => {
+    router.push(`${cluster.id}/dashboard`);
+  };
+  const graidients = [
+    'bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500',
+    'bg-gradient-to-r from-green-300 via-blue-500 to-purple-600',
+    'bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100',
+    'bg-gradient-to-r from-green-200 via-green-300 to-blue-500',
+    "bg-gradient-to-r from-green-300 via-yellow-300 to-pink-300"
+  ];
 
+  const random = Math.floor(Math.random() * 5) + 1;
 
   return (
-    <div onClick={routeToCluster} className="card bg-base-100 h-48 w-72 overflow-hidden rounded-xl shadow-xl hover:ring-4 ">
-      <figure className="w-full  ">
-        <img
-          src={cluster.img}
-          alt="cluster-image"
-          className="h-24 w-full object-cover"
-        />
+    <div
+      onClick={routeToCluster}
+      className='card h-48 w-72 overflow-hidden rounded-xl bg-base-100 shadow-xl hover:ring-4 '
+    >
+      <figure className='w-full  '>
+        <div className={`h-24 w-full object-cover ${graidients[random]}`} />
       </figure>
-      <div className="m-4 flex  w-full">
-        <h2 className="card-title ">{cluster.name}</h2>
+      <div className='m-4 flex  w-full'>
+        <h2 className='card-title '>{cluster.name}</h2>
       </div>
     </div>
   );
