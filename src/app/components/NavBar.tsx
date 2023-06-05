cff60bdc453b757f615b57d9f8e824d781e347d4
Ariel Lin <al2248@cornell.edu>
2023-06-05 13:46:08 -0400

    fixed profile picture displacement

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -11,25 +11,25 @@ const NavBar = () => {
   const { data: sessionData } = useSession();
 
   return (
-    <div className="navbar fixed top-0 left-0 right-0 bg-gray-100 bg-opacity-50 z-30 backdrop-blur-sm">
+    <div className="navbar flex justify-between fixed top-0 left-0 right-0 bg-gray-100 bg-opacity-50 z-30 backdrop-blur-sm">
       <div className="flex cursor-pointer flex-row align-middle">
         <Link href="/"><Image width="60" height="60" src={logo} alt="logo" className="mr-2 h-8 w-8" /></Link>
         <Link className="btn btn-ghost text-xl normal-case" href="/">Kafka Nimbus</Link>
       </div>
 
-      <div className="">
+      <div className="mr-5">
         {(!sessionData) ?
           <Link href="./api/auth/signin?callbackUrl=/cluster-dashboard"
             className="overflow-hidden hover:bg-slate-300"
-          ><Image width="50"
-            height="50"
+          ><Image width="35"
+            height="35"
             alt="logo-not-logged-in" src={"https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"}></Image></Link> :
           <details className="dropdown dropdown-end">
             <summary
               className="rounded-full overflow-hidden w-35 list-none">
               <Image
-                width="50"
-                height="50"
+                width="35"
+                height="35"
                 src={sessionData ? sessionData?.user?.image : "https://upload.wikimedia.org/wikipedia/commons/3/3f/Github-circle_%28CoreUI_Icons_v1.0.0%29.svg"}
                 alt="profile-pic"
                 className="rounded-full overflow-hidden  hover:bg-slate-300"
