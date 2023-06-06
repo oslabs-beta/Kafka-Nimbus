f8ff1bbc0faf69b5057d923066e5386c1b161f2c
Ariel Lin <al2248@cornell.edu>
2023-06-06 19:16:59 -0400

    edited error handler colors

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,14 +1,14 @@
 'use client';
 import Link from 'next/link';
 // global error handler
-export default function Error({error, reset} : {error: Error; reset: () => void}) {
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   return (
     <main className='grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8'>
       <div className='text-center'>
-        <p className='text-base font-semibold text-emerald-700 dark:text-emerald-500'>
+        <p className='text-base font-semibold text-red-700 dark:text-red-700'>
           There was a problem
         </p>
-        <h1 className='mt-4 text-3xl font-bold tracking-tight text-zinc-900 dark:text-zinc-50'>
+        <h1 className='mt-4 text-3xl font-bold tracking-tight text-zinc-900 dark:text-zinc-900'>
           {error.message || 'Something went wrong'}
         </h1>
         <p className='mt-6 text-base leading-7 text-zinc-600'>
