c678f0c2c0c18e766849e2afda26425196539c99
Andrew <androoo.1997@gmail.com>
2023-06-07 20:42:30 -0400

    added test video

--- src/app/components/Herpage-right.tsx
+++ src/app/components/Herpage-right.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { motion } from "framer-motion";
 import { useSession } from "next-auth/react";
-import Demovideo from "./Demovideo";
-import Link from "next/link";
-
+import Testvideo from "./InfoVideo";
 
 
 const Heroright = () => {
@@ -14,7 +11,7 @@ const Heroright = () => {
     <>
     <div className="hero z-0 min-h-10">
     <div className="hero-content flex-col lg:flex-row text-right">
-      <Demovideo />
+      <Testvideo />
       <div>
         <h1 className="text-5xl font-bold">
         Manage and View Advanced Metrics
@@ -23,15 +20,6 @@ const Heroright = () => {
         Monitor real-time health status of brokers by navigating through easy to use menu to view comprehensive cluster metadata to optimize cluster management.
 
         </p>
-        <Link href="/cluster-dashboard" className="mx-8 font-bold">
-          <motion.button
-            className="btn-primary btn "
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.8 }}
-          >
-            Get Started
-          </motion.button>
-        </Link>
       </div>
     </div>
   </div>
