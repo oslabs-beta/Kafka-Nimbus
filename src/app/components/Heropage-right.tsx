26abde8bc0a4e9ac7e89b737ac11586eb2ce79eb
Andrew <androoo.1997@gmail.com>
2023-06-07 21:53:29 -0400

    added link to docs in navbar

--- src/app/components/Heropage-right.tsx
+++ src/app/components/Heropage-right.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import { motion } from "framer-motion";
-import { useSession } from "next-auth/react";
 import Demovideo from "./Demovideo";
-import Link from "next/link";
-
-
 
 const Heroright = () => {
-
-  const { data: sessionData } = useSession();
   return (
     <>
     <div className="hero z-0 min-h-10">
@@ -23,15 +16,6 @@ const Heroright = () => {
         Monitor real-time health status of brokers by navigating through easy to use menu to view comprehensive cluster metadata to optimize cluster management.
 
         </p>
-        <Link href="/cluster-dashboard" className="mx-8 font-bold">
-          <motion.button
-            className="btn-primary btn "
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.8 }}
-          >
-            Get Started
-          </motion.button>
-        </Link>
       </div>
     </div>
   </div>
